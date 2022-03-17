c960b050caa122cb399a6dea06b416214cd81c13
fgnanni <flavio.nanni@mercadolivre.com>
2022-03-17 11:38:02 -0300

    Entendendo sobre o maven e inciando uma aplicacao springBoot

---
+++ springBoot/hello-spring/src/main/java/br/com/mercadolivre/estudoSpring/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.mercadolivre.estudoSpring;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String HelloWorld(){
+        return "Hello World";
+    }
+
+}
